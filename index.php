72531208cf0346fcac12c3c6a95da05efe870e1d
mharmonikas <38589714+mharmonikas@users.noreply.github.com>
2021-09-04 16:09:15 +0200

    Update index.php

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
 
 /*
  *---------------------------------------------------------------
- * APPLICATION ENVIRONMENT
+ * APPLICATION ENVIRONMENTs
  *---------------------------------------------------------------
  *
  * You can load different configurations depending on your
